b387e62c0fc39444a7524b13d28bb3bf004ff2da
shubham Kasar <forworkpurposesonly29@gmail.com>
2024-10-29 20:27:44 +0530

    Add files via upload

---
+++ lecture 1 (intro)/codagram.java
@@ -0,0 +1,11 @@
+public class codagram{
+    public static void main(String[] args) {
+          int a = 1;
+          int b = 2;
+
+
+         int  c = a + b; 
+
+          System.err.println(c);
+        }
+    }
